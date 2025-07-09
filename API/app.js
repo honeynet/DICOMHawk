90f0118b1a2e34ff2a5e9bdc9baec408a5105a65
Anshika Sharma <anshikasharmaa1517@gmail.com>
2025-07-10 01:43:09 +0530

    refactor: centralize logging system for TPot integration

--- API/app.js
+++ API/app.js
@@ -3,11 +3,10 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const multer = require('multer');
-logger = require("./logger.js")
+const logger = require('./logger.js');
 const app = express();
 app.set('trust proxy', true);
 app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')));
-
 
 //middleware
 app.use(express.json());
@@ -28,17 +27,12 @@ const csrfMiddleware = require("./middleware/csrfMiddleware.js");
 const sessionMiddleware = require('./middleware/sessionMiddleware.js');
 app.use(sessionMiddleware);
 
-
-
-
-
 const USERS = {
   username: 'test', password: 'test',
-}
+};
 
 // const cors = require('cors');
 // const { verifyCSRFToken } = require('./middleware/csrfMiddleware.js');
-
 
 //Routes
 const mainRoutes = require('./routes/main.routes.js');
@@ -50,23 +44,18 @@ app.use('/', adminRoutes);
 //const authRoutes = require('./routes/auth.routes.js');
 //app.use('/', authRoutes);
 
-
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
-  }
+  },
 });
 
 const upload = multer({ storage });
 
 app.post('/upload', (req, res) => {
-
-  
-  
   upload.array('dicomFiles[]', 1)(req, res, function (err) {
     if (err) {
       if (err.code === 'LIMIT_UNEXPECTED_FILE') {
@@ -83,21 +72,13 @@ app.post('/upload', (req, res) => {
       }))
     });
   });
-
 });
 
-
-
 app.post('/login', async (req, res) => {
-
-
   try {
-
     if (!req.body.username || !req.body.password) {
-      return res.status(401).json({ message: 'Empty username or password!' })
-    }
-
-
+      return res.status(401).json({ message: 'Empty username or password!' });
+    } 
     else if (req.body.username === "oolnie" && req.body.password === "iamtcte4*") {
       req.session.user = { username: req.body.username };
 
@@ -105,16 +86,13 @@ app.post('/login', async (req, res) => {
       const refreshToken = jwtUtils.generateRefreshToken({ username: req.body.username });
       res.cookie("accessToken", accessToken, { httpOnly: true, secure: false, sameSite: "Strict" });
       res.cookie("refreshToken", refreshToken, { httpOnly: true, secure: false, sameSite: "Strict" });
-      logger.logEvent("Hidden credintials accessed", req)
+      logger.logEvent("Hidden credentials accessed", req);
       res.status(200).json({ message: 'Login dev' });
       // Convert JSON object to a string
-
-    }
-
+    } 
     else if (req.body.username === USERS.username && req.body.password === USERS.password) {
-      logger.logEvent(`Successful login User ${req.body.username} and password ${req.body.password} `, req)
+      logger.logEvent(`Successful login User ${req.body.username} and password ${req.body.password} `, req);
       req.session.user = { username: req.body.username };
-      // console.log('User ','"',req.body.username,'"',' logged in. Session ID:', req.sessionID); // Log the session ID
 
       const accessToken = jwtUtils.generateAccessToken({ username: req.body.username });
       const refreshToken = jwtUtils.generateRefreshToken({ username: req.body.username });
@@ -125,29 +103,25 @@ app.post('/login', async (req, res) => {
       res.cookie("refreshToken", refreshToken, { httpOnly: true, secure: false, sameSite: "Strict" });
       // res.cookie("csrfToken", csrfToken, {httpOnly: false, secure: true, sameSite:"Strict"});
       // console.log("cookie", res.cookie);
-      //onsole.log("CSRF token:::", csrfToken);
-
+      // console.log("CSRF token:::", csrfToken);
 
       return res.status(200).json({ message: 'Login successful!' });
 
     } else {
       //it has been "invalid credentials before"
-      logger.logEvent(`Fail login User ${req.body.username} and password ${req.body.password}`, req)
+      logger.logEvent(`Fail login User ${req.body.username} and password ${req.body.password}`, req);
 
       return res.status(401).json({ message: 'Invalid username or password!' });
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-
 });
-
-
 
 app.get("/get-ip", (req, res) => {
   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-  const port = req.connection.remotePort
-  res.json({ ip,port });
+  const port = req.connection.remotePort;
+  res.json({ ip, port });
 });
 app.post('/logout', (req, res) => {
   req.session.destroy(err => {
@@ -158,7 +132,7 @@ app.post('/logout', (req, res) => {
     if (err) {
       return res.status(500).send('Error logging out');
     }
-  })
+  });
   res.clearCookie('accessToken');
   //console.log("aaaaaaaa",res.clearCookie('accessToken'))
   res.clearCookie('refreshToken');
@@ -186,23 +160,17 @@ app.get('/refresh', (req, res) => {
     res.cookie("refreshToken", newRefreshToken, { httpOnly: true, secure: false, sameSite: "Strict" });
 
     res.redirect(req.query.redirectFrom);
-
   } catch (error) {
     res.status(403).json({ message: "Invalid or expired refresh token" });
   }
-
 });
 
 app.get('/', async (req, res) => {
   if (req.session.user) {
-    res.sendFile((path.join(__dirname, 'static', 'main.html')));
+    res.sendFile(path.join(__dirname, 'static', 'main.html'));
   } else {
-    res.sendFile((path.join(__dirname, 'static', 'login.html')));
+    res.sendFile(path.join(__dirname, 'static', 'login.html'));
   }
-
-
-
-
 });
 
 //check-admin-token is called loginAdmin to avoid fingerprinting
@@ -214,7 +182,6 @@ app.get('/loginAdmin', (req, res) => {
     res.json({ isAdmin: false });
   }
 });
-
 
 // See all instances table has the option to download a single instance
 app.get('/downloadInstance', (req, res) => {
@@ -228,7 +195,7 @@ app.get('/downloadInstance', (req, res) => {
 
 // See all studies table has the option to download a zip folder
 app.get('/downloadStudyFiles', (req, res) => {
-  logger.logEvent("StudyFilesDownloaded",req)
+  logger.logEvent("StudyFilesDownloaded", req);
   const file = path.join(__dirname, './static/hf/StudyFiles.zip');
   res.download(file, 'StudyFiles.zip', (err) => {
     if (err) {
@@ -236,7 +203,6 @@ app.get('/downloadStudyFiles', (req, res) => {
     }
   });
 });
-
 
 //run the app
 try {
@@ -246,4 +212,3 @@ try {
 } catch (error) {
   console.error("Error. Server did not start", error);
 }
-
