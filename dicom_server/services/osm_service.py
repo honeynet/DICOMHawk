e56fb860793e6a557dfdec39f46800efb421da82
Ashutosh Kumar <130897584+codeaashu@users.noreply.github.com>
2025-07-14 17:33:52 +0530

    Create osm_service.py

---
+++ dicom_server/services/osm_service.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+from typing import List
+
+
+class IOSMService(ABC):
+
+    @abstractmethod
+    def get_medical_institutions(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def refresh_cache(self) -> bool:
+        pass
+
+    @abstractmethod
+    def is_cache_valid(self) -> bool:
+        pass 
