224e4e6423145596dd753c4d1a5af4f807be7c6e
Ashutosh Kumar <130897584+codeaashu@users.noreply.github.com>
2025-07-14 17:30:55 +0530

    Update tcia_management.py

--- dicom_server/custom_units/tcia_management.py
+++ dicom_server/custom_units/tcia_management.py
@@ -8,12 +8,13 @@ from services.dicom_database_service import IDicomDatabase
 from services.tci_services import ITCIAAPI
 from services.tci_services import ITCIAScheduler
 from services.tci_services import ITCIAManager
+from services.osm_service import IOSMService
 from dependency_injector.wiring import inject
 import logging
+import time
 
 
 class TCIAScheduler(threading.Thread, ITCIAScheduler):
-
     @inject
     def __init__(
         self,
@@ -30,24 +31,26 @@ class TCIAScheduler(threading.Thread, ITCIAScheduler):
         self.tcia_manager = tcia_manager or ITCIAManager()
         self.logger = app_logger
         self.start()
-
     def run(self):
         self.schedule_files_retrieval()
         self.logger.info(
-            f"TCIA retrieving schedule is started, DICOM files storage and database will be directly updated from The Cancer Imaging Archeive each {self.period} {self.period_unit}"
+            f"TCIA retrieving schedule is started, DICOM files storage and database will be directly updated from The Cancer Imaging Archive each {self.period} {self.period_unit}"
         )
+
+        while True:
+            schedule.run_pending()
+            time.sleep(10)  
 
     def schedule_files_retrieval(self):
         try:
             schedule_unit = getattr(schedule.every(self.period), str(self.period_unit))
-            schedule_unit.do(self.tcia_manager.change_dicom_files)
 
+
+            schedule_unit.do(self.tcia_manager.change_dicom_files)
         except Exception:
             self.exceptions_logger.exception(
                 "Unexpected error while running TCIA files retrieve scheduler"
             )
-
-
 class TCIAManager(ITCIAManager):
     def __init__(
         self,
@@ -61,6 +64,7 @@ class TCIAManager(ITCIAManager):
         dicomdb: IDicomDatabase = None,
         redis_handler: IRedisService = None,
         tcia_api: ITCIAAPI = None,
+        osm_service: IOSMService = None,
     ):
         self.honeytoken_url = honeytoken_url
         self.tcia_dir = tcia_dir
@@ -68,17 +72,16 @@ class TCIAManager(ITCIAManager):
         self.storage_directory = storage_directory
         self.dicomdb = dicomdb or IDicomDatabase()
         self.redis_handler = redis_handler or IRedisService()
+        self.osm_service = osm_service
         self.exceptions_logger = exceptions_logger
         self.logger = app_logger
 
         self.tcia_api = tcia_api
         self.pdf_canary_path = pdf_canary_path
         self.change_dicom_files_called = False
-
     def change_dicom_files(self):
         # to be used in unit testing
         self.change_dicom_files_called = True
-
         self.logger.info("Scheduled change of dicom files started")
         try:
             self.logger.debug("Stagging existing files")
@@ -87,28 +90,20 @@ class TCIAManager(ITCIAManager):
             )
             self.logger.debug("Getting access token from TCIA")
             self.tcia_api.get_access_token()
-
             self.logger.debug("New files retrieval")
             exist_studies = self.redis_handler.get_TCI_existing_studies()
-
             self.tcia_api.get_new_files(exist_studies, self.tcia_dir)
-
             self.logger.debug("Organizing downloaded files")
             self.organize_downloaded_files()
-
             self.logger.debug("Cleaning up staged files")
             tcia_util.delete_staged_files(self.stagger_dir)
-
             self.logger.debug("Updating DICOM database")
             self.dicomdb.initialize_database()
-
         except Exception:
             self.exceptions_logger.exception(
                 "Changing Dicom files with TCIA files failed: Rolling back changes"
             )
-
             self.roll_back_changes()
-
     def roll_back_changes(self):
         self.logger.debug("Roll-back: Deleting downloaded files")
         tcia_util.delete_downloded_files_if_exist(self.tcia_dir)
@@ -116,7 +111,6 @@ class TCIAManager(ITCIAManager):
         tcia_util.restore_old_files(
             self.storage_directory, self.tcia_dir, self.stagger_dir
         )
-
     def organize_downloaded_files(self):
         self.logger.info("Organizing TCIA retrieved files")
         directory = tcia_util.initialize_dicom_directory_if_not_exist(
@@ -130,11 +124,9 @@ class TCIAManager(ITCIAManager):
                 modality, self.tcia_dir
             ):
                 try:
-
                     self.redis_handler.add_TCI_study(study_uid)
                 except Exception:
                     self.exceptions_logger.exception("Study addition failed:")
-
                 study_files_counter = 0
                 (
                     patient_name,
@@ -145,7 +137,7 @@ class TCIAManager(ITCIAManager):
                     study_date,
                     accession_number,
                 ) = tcia_util.generate_patient_info()
-                institution = tcia_util.get_random_institution()
+                institution = tcia_util.get_random_institution(self.osm_service)
                 for se_uid in tcia_util.get_downloaded_series_per_study(
                     modality, study_uid, self.tcia_dir
                 ):
@@ -171,7 +163,6 @@ class TCIAManager(ITCIAManager):
                             se_uid,
                             file,
                         )
-
     def process_serie_file(
         self,
         directory,
@@ -191,7 +182,6 @@ class TCIAManager(ITCIAManager):
         file,
     ):
         if not tcia_util.is_licience_file(file):
-
             dataset = tcia_util.build_file_dataset(
                 modality,
                 study_uid,
@@ -215,11 +205,8 @@ class TCIAManager(ITCIAManager):
             ):
                 # Injecting 4 retrieved dicom file with canary token and honeyURL token
                 self.inject_honey_url(dataset)
-
                 self.inject_pdf_canary_token(dataset)
-
                 self.redis_handler.add_injected_file(patient_name, modality)
-
             tcia_util.store_retrieved_file(
                 directory,
                 modality,
@@ -227,7 +214,6 @@ class TCIAManager(ITCIAManager):
                 patient_name,
                 dataset,
             )
-
     def inject_pdf_canary_token(self, dataset):
         try:
             self.logger.debug(
@@ -240,7 +226,6 @@ class TCIAManager(ITCIAManager):
             self.exceptions_logger.exception(
                 "Unexpected error while injecting canary token"
             )
-
     def inject_honey_url(self, dataset):
         try:
             dataset.RetrieveURL = (
@@ -253,7 +238,6 @@ class TCIAManager(ITCIAManager):
             self.exceptions_logger.exception(
                 f"Unexpected error while injecting HoneyURL for Patient {dataset.PatientName}, modality: {dataset.Modality}"
             )
-
     def get_canary_token(self):
         pdf_path = self.pdf_canary_path
         try:
@@ -262,10 +246,7 @@ class TCIAManager(ITCIAManager):
                 return pdf_data
         except Exception:
             self.exceptions_logger.exception("Canary token retrieval failed:")
-
-
 class TCIAAPI(ITCIAAPI):
-
     def __init__(
         self,
         app_logger,
@@ -285,11 +266,9 @@ class TCIAAPI(ITCIAAPI):
         self.number_of_studies_in_each_retrieved_modality = studies_per_mod
         self.modalities = modalities
         self.logger = app_logger
-
     def get_new_files(self, existing_studies, tcia_dir):
         self.logger.debug("Calling TCIA API")
         try:
-
             _json = {}
             for mod in self.modalities:
                 metadata = {}
@@ -304,7 +283,6 @@ class TCIAAPI(ITCIAAPI):
                         self.minimum_files_in_each_retrieved_serie,
                         self.maximum_files_in_each_retrieved_serie,
                     )
-
                     for st_uid, se_uids in metadata.items():
                         for se_uid_dict in se_uids:
                             se_uid = se_uid_dict["se_uid"]
@@ -316,14 +294,12 @@ class TCIAAPI(ITCIAAPI):
                                 },
                             )
                             response.raise_for_status()
-
                             tcia_util.extract_and_save_zip_data(
                                 mod, st_uid, se_uid, response, tcia_dir
                             )
             metadata = {}
         except Exception:
             self.exceptions_logger.exception("New files retrieval failed:")
-
     def get_access_token(self):
         session = requests.Session()
         adapter = requests.adapters.HTTPAdapter(max_retries=3)
@@ -342,10 +318,8 @@ class TCIAAPI(ITCIAAPI):
             json_object = json.loads(res.content)
             self.access_token = json_object["access_token"]
             self.refresh_token = json_object["refresh_token"]
-
         except Exception:
             self.exceptions_logger.exception("Access token retrieval failed:")
-
     def refresh_access_token(self):
         session = requests.Session()
         adapter = requests.adapters.HTTPAdapter(max_retries=3)
@@ -362,7 +336,6 @@ class TCIAAPI(ITCIAAPI):
             )
         except Exception:
             self.exceptions_logger.exception("Token refresh failed:")
-
     def get_studies_based_on_modalities(self, mod):
         session = requests.Session()
         adapter = requests.adapters.HTTPAdapter(max_retries=3)
